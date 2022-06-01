69be70bfc0e77c2d2b24c888e916b9dfa878bcbe
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-06-01 09:16:01 +0430

    21210 done

--- 21210/postal_card/templatetags/filters.py
+++ 21210/postal_card/templatetags/filters.py
@@ -1,1 +1,15 @@
-# Put you code here
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+
+@register.filter
+@stringfilter
+def fa_num(value: str):
+    fa = "۰۱۲۳۴۵۶۷۸۹"
+    res = value
+    for c in value:
+        if c.isnumeric():
+            res = res.replace(c, fa[int(c)])
+    return res
