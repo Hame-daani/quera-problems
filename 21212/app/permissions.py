3c7e5e84462fe1778c645d4271b21c794a25b066
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-06-02 15:48:49 +0430

    21212 done

--- 21212/app/permissions.py
+++ 21212/app/permissions.py
@@ -2,4 +2,7 @@ from rest_framework import permissions
 
 
 class IsOwnerOrAdminOrReadOnly(permissions.BasePermission):
-    pass
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_superuser or obj.owner == request.user
--- 21212/app/serializers.py
+++ 21212/app/serializers.py
@@ -1,10 +1,39 @@
-class PostSerializer:
-    pass
+from rest_framework import serializers
+from app.models import Comment, Post
 
 
-class PostDetailSerializer:
-    pass
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Post
+        fields = ("title", "body", "created", "owner")
+
+    def get_owner(self, obj):
+        return obj.owner.username
 
 
-class CommentSerializer:
-    pass
+class PostDetailSerializer(serializers.ModelSerializer):
+    owner = serializers.SerializerMethodField()
+    comment_set = serializers.HyperlinkedRelatedField(
+        many=True, read_only=True, view_name="comment_detail"
+    )
+
+    class Meta:
+        model = Post
+        fields = ("title", "body", "created", "updated", "owner", "comment_set")
+
+    def get_owner(self, obj):
+        return obj.owner.username
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.SerializerMethodField()
+    post = serializers.HyperlinkedRelatedField(read_only=True, view_name="post_detail")
+
+    class Meta:
+        model = Comment
+        fields = ("post", "owner", "body", "created", "updated")
+
+    def get_owner(self, obj):
+        return obj.owner.username
