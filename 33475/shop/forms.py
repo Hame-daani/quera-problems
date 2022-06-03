1cf1f49dd70a25a4edfe07ad54a29048d11cced6
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-06-03 10:53:07 +0430

    33475 initial

---
+++ 33475/shop/forms.py
@@ -0,0 +1,16 @@
+import json
+
+from django import forms
+
+from shop.models import Product
+
+
+class CartForm(forms.Form):
+    # delete "pass" and write your code here
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def clean(self):
+        pass
+	
+        خ
