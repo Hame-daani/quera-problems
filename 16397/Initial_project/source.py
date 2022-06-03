a0e9d3b5df73fc2681c8699dee7273b34c441cd1
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-06-03 20:25:07 +0430

    16397 done

--- 16397/Initial_project/source.py
+++ 16397/Initial_project/source.py
@@ -1,25 +1,40 @@
 class Grade:
     def __init__(self, stu_id, crc_code, score):
-        self.student_id = stu_id
-        self.course_code = crc_code
-        self.score = score
+        self.student_id = int(stu_id)
+        self.course_code = int(crc_code)
+        self.score = float(score)
 
 
 class CourseUtil:
     def set_file(self, address):
-        pass
+        self.file = address
+        self.load_all()
+
+    def load_all(self):
+        with open(self.file, "r") as f:
+            lines = f.readlines()
+            self.all_grades = [Grade(*line.strip("\n").split(" ")) for line in lines]
 
     def load(self, line_number):
-        pass
+        return self.all_grades[line_number - 1]
 
     def calc_student_average(self, student_id):
-        pass
+        std_grades = [g.score for g in self.all_grades if g.student_id == student_id]
+        return sum(std_grades) / len(std_grades)
 
     def calc_course_average(self, course_code):
-        pass
+        course_grades = [
+            g.score for g in self.all_grades if g.course_code == course_code
+        ]
+        return sum(course_grades) / len(course_grades)
 
     def count(self):
-        pass
+        return len(self.all_grades)
 
-    def save(self, grade):
-        pass
+    def save(self, grade: Grade):
+        for g in self.all_grades:
+            if grade.student_id == g.student_id and grade.course_code == g.course_code:
+                return
+        with open(self.file, "a") as f:
+            f.write(f"\n{grade.student_id} {grade.course_code} {grade.score}")
+        self.load_all()
