280236cfe5423ba3a690232a0c3879d986695587
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-06-03 16:55:03 +0430

    16396 done

--- 16396/Initial_project/source.py
+++ 16396/Initial_project/source.py
@@ -1,2 +1,14 @@
 class Foo:
-    pass
+    def __init__(self):
+        self._x = 0
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        if value >= 0:
+            self._x = value % 100
+        else:
+            self._x = -1
