460d6efd06d40df13bd69293eee20d021a659844
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-06-03 12:16:27 +0430

    16401 initial

---
+++ 16401/cabin/queries.py
@@ -0,0 +1,86 @@
+from .models import *
+
+
+def query_0():
+    q = Driver.objects.all()
+    return q
+
+
+def query_1():
+    """
+    :return: Something like {'income': value }
+    """
+    q = 'your query here'
+    return q
+
+
+def query_2(x):
+    """
+    :return: Something like {'payment_sum': value }
+    """
+    q = 'your query here'
+    return q
+
+
+def query_3():
+    """
+    :return: Just a number
+    """
+    q = 'your query here'
+    return q
+
+
+def query_4():
+    q = 'your query here'
+    return q
+
+
+def query_5(t):
+    q = 'your query here'
+    return q
+
+
+def query_6():
+    """
+    :return: Account object
+    """
+    q = 'your query here'
+    return q
+
+
+def query_7():
+    q = 'your query here'
+    return q
+
+
+def query_8(x):
+    q = 'your query here'
+    return q
+
+
+def query_9():
+    q = 'your query here'
+    return q
+
+
+def query_10():
+    q = 'your query here'
+    return q
+
+
+def query_11(n, c):
+    q = 'your query here'
+    return q
+
+
+def query_12(n, c):
+    q = 'your query here'
+    return q
+
+
+def query_13(n, m):
+    """
+    :return: Something like {'sum_duration': value }
+    """
+    q = 'your query here'
+    return q
