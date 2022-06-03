ae51884b3e7248d7f01d2ec450e953952ad1a173
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-06-03 16:29:00 +0430

    21205 done

--- 21205/Initial_project/source.py
+++ 21205/Initial_project/source.py
@@ -1,2 +1,16 @@
 def check_registration_rules(**kwargs):
-    pass
+    blacklist = ["quera", "codecup"]
+    min_uesrname_len = 4
+    min_password_len = 6
+    allowed = []
+    for username, password in kwargs.items():
+        if username in blacklist:
+            continue
+        if len(username) < min_uesrname_len:
+            continue
+        if len(password) < min_password_len:
+            continue
+        if password.isnumeric():
+            continue
+        allowed.append(username)
+    return allowed
