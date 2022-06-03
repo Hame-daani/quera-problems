8fd2671a811e56830c9b81bc782fd617d7d67c6c
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-06-03 16:44:18 +0430

    33473 done

--- 33473/Initial_project/solution.py
+++ 33473/Initial_project/solution.py
@@ -1,2 +1,21 @@
+from math import pi
+
+
+def square(a):
+    return a * a
+
+
+def circle(r):
+    return pi * r * r
+
+
+def rectangle(a, b):
+    return a * b
+
+
+def triangle(a, b):
+    return (1 / 2) * a * b
+
+
 def get_func(ls):
-    pass
+    return [globals()[l] for l in ls]
