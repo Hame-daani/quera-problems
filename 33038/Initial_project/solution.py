237a1320986389d001d335e2d908007b2166d735
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-06-03 17:37:05 +0430

    33038 done

--- 33038/Initial_project/solution.py
+++ 33038/Initial_project/solution.py
@@ -1,6 +1,16 @@
 class ExceptionProxy(Exception):
-    pass
+    def __init__(self, e, f):
+        self.msg = e
+        self.function = f
 
 
 def transform_exceptions(func_ls):
-    pass
+    tr_ls = []
+    for func in func_ls:
+        try:
+            func()
+            tr = ExceptionProxy("ok!", func)
+        except Exception as e:
+            tr = ExceptionProxy(str(e), func)
+        tr_ls.append(tr)
+    return tr_ls
