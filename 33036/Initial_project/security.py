46c54ae5e72632cc47fa3d8a00bd4be3267815d1
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-06-04 12:15:58 +0430

    33036 done

--- 33036/Initial_project/security.py
+++ 33036/Initial_project/security.py
@@ -1,11 +1,47 @@
+import re
+
+
 class Security:
-    
     def secure(self, info):
-        pass
+        res = []
+        for text in info.split(" "):
+            if self.is_social_account_info(text):
+                account = text.split("/")[1]
+                text = text.replace(account, self.encrypt(account))
+            res.append(text)
+        return " ".join(res)
 
     def is_social_account_info(self, param):
-        pass
+        reg = r"[A-Z][a-z]+:www\.[a-z\d.]+\/[a-z\d_]+"
+        if re.fullmatch(reg, param):
+            return True
+        else:
+            return False
+
+    def _get_uniform(self, text):
+        result = []
+        prev = ""
+        curr = ""
+        for c in text:
+            if c == prev:
+                curr += c
+            else:
+                if curr:
+                    result.append(curr)
+                prev = c
+                curr = c
+        result.append(curr)
+        return result
+
+    def _get_weight(self, text):
+        w = ""
+        z = 1
+        for c in text:
+            w += str((ord(c) - 96) * z)
+            z += 1
+        return w
 
     def encrypt(self, s):
-        pass
-
+        uniforms = self._get_uniform(s)
+        weights = [self._get_weight(u) for u in uniforms]
+        return "".join(weights)
