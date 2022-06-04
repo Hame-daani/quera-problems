da45861c3e9f0f96fed0abcfa6f2d8180c8b1f3f
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-06-04 14:26:01 +0430

    21207 initial

---
+++ 21207/Initial_project/source.py
@@ -0,0 +1,19 @@
+import os
+from shutil import copyfile
+
+
+class FileManager:
+    def find(self, name, address):
+        pass
+
+    def create_file(self, name, address):
+        pass
+
+    def create_dir(self, name, address):
+        pass
+
+    def delete(self, name, address):
+        pass
+
+    def restore(self, name):
+        pass
