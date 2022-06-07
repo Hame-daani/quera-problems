7184edc3eeaac1075ea2c8e8ecb1b00247a9040c
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-06-07 12:20:26 +0430

    33040 done

--- 33040/Initial_project/solution.py
+++ 33040/Initial_project/solution.py
@@ -1,4 +1,29 @@
+import re
 import functions
+from threading import Thread
+
+
+def calculate(i):
+    try:
+        functions.f[i - 1]()
+        functions.g[i - 1]()
+        functions.h[i - 1]()
+        return
+    except:
+        return
+
 
 def solve():
-    pass
+    t1 = Thread(target=calculate, args=(1,), name="1")
+    t2 = Thread(target=calculate, args=(2,), name="2")
+    t3 = Thread(target=calculate, args=(3,), name="3")
+    t4 = Thread(target=calculate, args=(4,), name="4")
+    t4.start()
+    t3.start()
+    t2.start()
+    t1.start()
+    t4.join()
+    t3.join()
+    t2.join()
+    t1.join()
+    return
