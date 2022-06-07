6aede82e1eae2c1fdf8ffa08ef496c2deadba228
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-06-07 17:04:17 +0430

    21208 done

--- 21208/Initial_project/proxy.py
+++ 21208/Initial_project/proxy.py
@@ -1,17 +1,39 @@
-class Proxy:
+class Proxy(object):
     def __init__(self, obj):
-        self._obj = obj
+        object.__setattr__(self, "_obj", obj)
+        object.__setattr__(self, "call_stack", [])
+
+    def __getattribute__(self, name):
+        try:
+            return object.__getattribute__(self, name)
+        except:
+            try:
+                attr = getattr(self._obj, name)
+                self.call_stack.append(name)
+                return attr
+            except:
+                raise Exception("No Such Method")
+
+    def __delattr__(self, name):
+        delattr(object.__getattribute__(self, "_obj"), name)
+
+    def __setattr__(self, name, value):
+        setattr(object.__getattribute__(self, "_obj"), name, value)
 
     def last_invoked_method(self):
-        pass
+        try:
+            return self.call_stack[-1]
+        except:
+            raise Exception("No Method Is Invoked")
 
     def count_of_calls(self, method_name):
-        pass
+        return self.call_stack.count(method_name)
 
     def was_called(self, method_name):
-        pass
+        return method_name in self.call_stack
 
-class Radio():
+
+class Radio:
     def __init__(self):
         self._channel = None
         self.is_on = False
@@ -27,4 +49,3 @@ class Radio():
 
     def power(self):
         self.is_on = not self.is_on
-
