26f0c8d84f481f960a586a46464ace50b9be3f0f
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-06-07 17:19:31 +0430

    136630 done

--- 136630/solution.py
+++ 136630/solution.py
@@ -1,4 +1,6 @@
 def sort_dependencies(packages, package_name):
     package_dependencies = []
-
-    return package_dependencies
+    for pkg in packages[package_name]:
+        package_dependencies.extend(sort_dependencies(packages, pkg))
+    package_dependencies.extend(packages[package_name])
+    return list(dict.fromkeys(package_dependencies))
