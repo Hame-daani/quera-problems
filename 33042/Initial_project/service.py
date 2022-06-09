909b7761bc079ed441df80c0b514a8c56c1aae3f
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-06-09 11:45:38 +0430

    33042 initial

---
+++ 33042/Initial_project/service.py
@@ -0,0 +1,14 @@
+from material import TrafficUsageDao
+
+
+class TrafficUsageService:
+    def __init__(self, traffic_usage_dao: TrafficUsageDao):
+        self.traffic_usage_dao = traffic_usage_dao
+
+    def social_media_lovers(self, year: int, month: int):
+        # Write your codes here
+        pass
+
+    def download_lovers(self, year: int, month: int):
+        # Write your codes here
+        pass
